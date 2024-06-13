9bbe68593497ba96714945815a18b646aa1db436
SamShep2495 <samsheppard2495@hotmail.com>
2024-06-13 15:50:11 +0100

    The start of my new app

---
+++ app/(tabs)/football_shirts.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const footballShirts = () => {
+  return (
+    <View>
+      <Text>Football Shirts</Text>
+    </View>
+  )
+}
+
+export default footballShirts
+
+const styles = StyleSheet.create({})
